e30a9e0d5b53d3f936cf9d8b7778378077fdc38b
Matt York <york.matt@gmail.com>
2015-09-19 16:25:39 -0700

    fix typo

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-// / <reference path="node_modules/reflect-metadata/reflect-metadata.d.ts" />
+/// <reference path="node_modules/reflect-metadata/reflect-metadata.d.ts" />
 
 import 'reflect-metadata';
